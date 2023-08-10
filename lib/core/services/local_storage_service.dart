7456a87dd5227249a575f273e6c953f975e4c164
Waleed Alrashed <wmr1210@hotmail.com>
2023-08-10 11:30:59 +0400

    Update local_storage_service.dart

--- lib/core/services/local_storage_service.dart
+++ lib/core/services/local_storage_service.dart
@@ -1,21 +1,27 @@
+import 'package:countries_navigator/core/services/services.dart';
 import 'package:countries_navigator/core/services/services_locator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:countries_navigator/core/services/services.dart';
 
+///
+///
+///
 class LocalStorageService {
+  ///
+  ///Constructor
+  ///
+  LocalStorageService({required SharedPreferences preferences}) {
+    _logger.i('LocalStorageService(): init fun');
+    _preferences = preferences;
+  }
   static final _logger = locator<LogService>();
 
   static late SharedPreferences _preferences;
 
-  LocalStorageService({var preferences}) {
-    _logger.i("LocalStorageService(): init fun");
-    _preferences = preferences;
-  }
-
-  // updated _saveToDisk function that handles all types
+  /// updated _saveToDisk function that handles all types
   void saveToDisk<T>(String key, T content) {
     _logger.log.i(
-        '(TRACE) LocalStorageService:_saveToDisk. key: $key value: $content');
+      '(TRACE) LocalStorageService:_saveToDisk. key: $key value: $content',
+    );
 
     if (content is String) {
       _preferences.setString(key, content);
@@ -34,20 +40,26 @@ class LocalStorageService {
     }
   }
 
-  // updated _saveToDisk function that handles all types
+  /// updated _removeFromDisk function that handles all types
   Future<bool> removeFromDisk<T>(String key) {
     _logger.log.i('(TRACE) LocalStorageService:Remove from desk. key: $key');
 
     return _preferences.remove(key);
   }
 
+  ///
+  /// get value from disk
+  ///
   dynamic getFromDisk(String key) {
-    var value = _preferences.get(key);
+    final value = _preferences.get(key);
     _logger.log
         .i('(TRACE) LocalStorageService:_getFromDisk. key: $key value: $value');
     return value;
   }
 
+  ///
+  /// clear the entire storage
+  ///
   Future<bool> clearStorage() {
     _logger.log.i('(TRACE) LocalStorageService:clearStorage');
     return _preferences.clear();
